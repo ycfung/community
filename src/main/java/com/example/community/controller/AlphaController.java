9e7caed8b44742cc25707dac9013ac72b496ad12
Chingfung Yung <rcf@hotmail.co.th>
2022-08-01 21:15:44 +0800

    finished 1.4

--- src/main/java/com/example/community/controller/AlphaController.java
+++ src/main/java/com/example/community/controller/AlphaController.java
@@ -3,12 +3,14 @@ package com.example.community.controller;
 import com.example.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
+import java.io.PrintWriter;
+import java.util.*;
 
 @Controller
 @RequestMapping("/alpha")
@@ -44,6 +46,84 @@ public class AlphaController {
         // return response data
         response.setContentType("text/html;charset=UTF-8");
 
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write("<h1>HEAD</h1>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // GET request
+    @RequestMapping(path = "/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(@RequestParam(name = "current", required = false, defaultValue = "1") int current,
+                              @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "some students";
+    }
+
+    // path variable
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "student " + id;
+    }
+
+    // post request
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "student " + name + "<br>age " + age;
+    }
+
+    // response HTML data
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "zhangsan");
+        modelAndView.addObject("age", 30);
+        modelAndView.setViewName("/demo/view"); // equivalent to view.html
+        return modelAndView;
+
+    }
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name", "PKU");
+        model.addAttribute("age", 80);
+        return "/demo/view";
+
+    }
+
+    // response JSON data (asynchronous request)
+    // Java object -> JSON string -> JavaScript object
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, Object>> getEmp() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "zhangsan");
+        map.put("age", 30);
+        map.put("salary", 8000.00);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("name", "lisi");
+        map.put("age", 24);
+        map.put("salary", 9000.00);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("name", "wangwu");
+        map.put("age", 25);
+        map.put("salary", 10000.00);
+        list.add(map);
+
+        return list;
     }
 
 
