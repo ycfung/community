60d8003a0bf5f0ed6e32b686ee78e6fd98ebd57b
Bingzhen Chen <aa1157954743@gmail.com>
2022-08-01 17:18:10 +0800

    Initial commit

---
+++ src/main/java/com/example/community/service/AlphaService.java
@@ -0,0 +1,35 @@
+package com.example.community.service;
+
+import com.example.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+//@Scope("prototype")
+public class AlphaService {
+
+    @Autowired
+    private AlphaDao alphaDao;
+
+    public AlphaService() {
+        System.out.println("Instantiate AlphaService");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Initialize AlphaService");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroy AlphaService");
+    }
+
+    public String find() {
+        return alphaDao.select();
+    }
+}
